a885a0c88e574cd32e5b34bb23f392ce9ab675ea
Kevin Lam <mtn013209@gmail.com>
2025-06-08 14:56:38 -0700

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from "next/image"
+import styles from "./page.module.css"
 
 export default function Home() {
   return (
@@ -17,7 +17,7 @@ export default function Home() {
           <li>
             Get started by editing <code>app/page.js</code>.
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Save and see your changes instantly. a</li>
         </ol>
 
         <div className={styles.ctas}>
@@ -91,5 +91,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
