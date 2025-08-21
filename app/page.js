f52e0826d98beef4613c859ec3facfbb4d4ca112
Kevin Lam <mtn013209@gmail.com>
2025-08-20 18:03:31 -0700

    under construction placeholder

--- app/page.js
+++ app/page.js
@@ -1,10 +1,18 @@
 export default function Home() {
   return (
-    <div>
-      <main>
-        <div>Christy Lam Julian Foundation Landing Page Start</div>
-        <div>{Date.now()}</div>
-      </main>
-    </div>
+    <main>
+      <section className="section is-medium">
+        <p className="title has-text-white-ter has-text-centered">
+          Christy Lam Julian Foundation
+        </p>
+
+        <div className="has-text-white-ter has-text-centered is-size-4">
+          Under construction
+        </div>
+        <div className="has-text-white-ter has-text-centered is-size-4">
+          Send inquiries to: christy@lamjulianfoundation.org
+        </div>
+      </section>
+    </main>
   )
 }
